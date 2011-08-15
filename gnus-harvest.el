c190c15323d3811aa00928f1c585ed9879a0f99c
John Wiegley <johnw@newartisans.com>
2011-08-15 14:39:30 -0400

    First working version of the code

--- gnus-harvest.el
+++ gnus-harvest.el
@@ -25,6 +25,143 @@
 
 ;;; Commentary:
 
+;; This code requires that SQLite3 be installed.  Check to see if the command
+;; "sqlite3" is already available on your system.
+
+(require 'gnus)
+
+(defgroup gnus-harvest nil
+  ""
+  :group 'gnus)
+
+(defcustom gnus-harvest-sqlite-program (executable-find "sqlite3")
+  ""
+  :type 'file
+  :group 'gnus-harvest)
+
+(defcustom gnus-harvest-db-path (expand-file-name ".addrs" gnus-home-directory)
+  ""
+  :type 'file
+  :group 'gnus-harvest)
+
+(defcustom gnus-harvest-query-limit 50
+  ""
+  :type 'integer
+  :group 'gnus-harvest)
+
+(defun gnus-harvest-sqlite-invoke (sql &optional ignore-output-p)
+  (let ((tmp-buf (and (not ignore-output-p)
+                      (generate-new-buffer "*sqlite*"))))
+    (if sql
+        (call-process gnus-harvest-sqlite-program
+                      nil tmp-buf nil "-noheader" "-list"
+                      gnus-harvest-db-path sql)
+      (call-process-region (point-min) (point-max)
+                           gnus-harvest-sqlite-program
+                           nil tmp-buf nil "-noheader" "-list"
+                           gnus-harvest-db-path))
+    (unless ignore-output-p
+      (with-current-buffer tmp-buf
+        (prog1
+            (buffer-string)
+          (kill-buffer (current-buffer)))))))
+
+(defun gnus-harvest-create-db ()
+  (gnus-harvest-sqlite-invoke "
+CREATE TABLE
+    addrs
+    (
+        email TEXT(255) NOT NULL,
+        fullname TEXT(255),
+        last_seen INTEGER NOT NULL,
+        PRIMARY KEY (email),
+        UNIQUE (email)
+    )
+" t))
+
+(defun gnus-harvest-complete-stub (stub &optional prefix-only-p)
+  (read (concat "("
+                (gnus-harvest-sqlite-invoke
+                 (format "
+SELECT
+    '\"' ||
+    CASE
+        WHEN fullname IS NOT NULL
+        THEN fullname || ' <' || email || '>'
+        ELSE email
+    END
+    || '\"'
+FROM
+    (
+        SELECT
+            email, fullname, last_seen
+        FROM
+            addrs
+        WHERE
+            (email LIKE '%s%s%%' OR fullname LIKE '%s%s%%')
+        ORDER BY
+            last_seen DESC
+        LIMIT
+            %d
+    )"
+                         (if prefix-only-p "" "%") stub
+                         (if prefix-only-p "" "%") stub
+                         gnus-harvest-query-limit))
+                ")")))
+
+(defun gnus-harvest-insert-address (email fullname moment)
+  (insert "INSERT OR REPLACE INTO addrs (email, ")
+  (if fullname
+      (insert "fullname, "))
+  (insert "last_seen) VALUES (lower('" email "'), '")
+  (if fullname
+      (insert fullname "', '"))
+  (insert moment "');\n"))
+
+(defun gnus-harvest-addresses ()
+  "Harvest and remember the addresses in the current article buffer."
+  (let ((tmp-buf (generate-new-buffer "*gnus harvest*"))
+        (moment (number-to-string (floor (float-time)))))
+    (mapc (lambda (info)
+            (with-current-buffer tmp-buf
+              (gnus-harvest-insert-address (cadr info) (car info) moment)))
+          (delete
+           nil
+           (append
+            (mapcar (lambda (field)
+                      (let ((value (message-field-value field t)))
+                        (and value
+                             (mail-extract-address-components value))))
+                    '("to" "reply-to" "from" "resent-from" "cc" "bcc")))))
+    (with-current-buffer tmp-buf
+      (gnus-harvest-sqlite-invoke nil t)
+      (kill-buffer (current-buffer)))))
+
+(defun gnus-harvest-find-address ()
+  (interactive)
+  (backward-kill-word 1)
+  (insert
+   (let ((stub (word-at-point)))
+     (ido-completing-read "Use address: "
+                          (gnus-harvest-complete-stub stub)
+                          nil t stub))))
+
+(defun gnus-harvest-install (&rest features)
+  (unless (file-readable-p gnus-harvest-db-path)
+    (gnus-harvest-create-db))
+
+  (add-hook 'gnus-article-prepare-hook 'gnus-harvest-addresses)
+  (add-hook 'message-send-hook 'gnus-harvest-addresses)
+
+  (dolist (feature features)
+    (cond ((eq 'message-x feature)
+           (eval-after-load "message"
+             '(require 'message-x))
+           (eval-after-load "message-x"
+             '(add-to-list 'message-x-completion-alist
+                           '("\\([rR]esent-\\|[rR]eply-\\)?[tT]o:\\|[bB]?[cC][cC]:" .
+                             gnus-harvest-find-address)))))))
+
 (provide 'gnus-harvest)
 
 ;;; gnus-harvest.el ends here
