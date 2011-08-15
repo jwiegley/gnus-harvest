04666e68b707714b4d3ada0bf2389ff1ab45b53e
John Wiegley <johnw@newartisans.com>
2011-08-15 10:57:43 -0400

    Start of project

---
+++ gnus-harvest.el
@@ -0,0 +1,30 @@
+;;; gnus-harvest --- Harvest e-mail address from read/written articles
+
+;; Copyright (C) 2011 John Wiegley
+
+;; Author: John Wiegley <johnw@gnu.org>
+;; Created: 15 Aug 2011
+;; Version: 1.0
+;; Keywords: gnus email
+;; X-URL: https://github.com/jwiegley/gnus-harvest
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+(provide 'gnus-harvest)
+
+;;; gnus-harvest.el ends here
